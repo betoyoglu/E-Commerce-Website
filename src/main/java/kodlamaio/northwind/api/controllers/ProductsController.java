ed0d59cd48a8c205715035c2c5593a20480ec400
Betül Toyoğlu <toyoglubetul@gmail.com>
2024-08-22 13:05:59 +0300

    product with category dto

--- src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -14,6 +14,7 @@ import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 @RestController // Bu sınıfın bir REST denetleyicisi olduğunu belirtir.yani hem ios hem android için çalışır
 @RequestMapping("/api/products") // Bu denetleyicinin temel URL yolunu belirler. Tüm yollar /api/products ile başlar.
@@ -61,4 +62,10 @@ public class ProductsController {
 	public DataResult<List<Product>> getAllSorted(){
 		return this.productService.getAllSorted();
 	}
+	
+	@GetMapping("/getProductWithCategoryDetails") 
+	public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails(){
+		return this.productService.getProductWithCategoryDetails();
+	}
+	
 }
